9db0f4962a2c75da3f488c077b33588cfe82b1fd
Embedded-Solution <63601771+Embedded-Solution@users.noreply.github.com>
2023-06-05 21:56:13 +0200

    reset default settings

--- src/Settings.hpp
+++ src/Settings.hpp
@@ -9,8 +9,8 @@
 #define FIRMWARE_VERSION 3.6
 
 // Debug parameters
- #define MODE_DEBUG
- #define SERIAL1_DEBUG_OUTPUT
+// #define MODE_DEBUG
+// #define SERIAL1_DEBUG_OUTPUT
 
 // Dives parameters
 #define OFFSET_SLEEP_STATIC 1700         /*Offset going to sleep and wake up static mode*/
@@ -24,12 +24,12 @@
 #define MAX_DYNAMIC_COUNTER_NO_DIVE 600  /* Number of No Water to end dynamic dive (TIME_DYNAMIC_MODE / 1000 * MAX_DYNAMIC_COUNTER = seconds)*/
 #define MAX_STATIC_COUNTER 2             /* Number of No Water to end static dive (TIME_TO_SLEEP_STATIC * MAX_STATIC_COUNTER = seconds )*/
 #define WATER_TRIGGER 1500               // mV water detection level (0 to 3300) (lower value for pure water)
-#define TIME_GPS_START 5               // research time gps at the beginning of the dive(seconds)
-#define TIME_GPS_END 5                 // research time gps at the end of the dive (seconds)
+#define TIME_GPS_START 600               // research time gps at the beginning of the dive(seconds)
+#define TIME_GPS_END 900                 // research time gps at the end of the dive (seconds)
 #define TIME_SURFACE_DETECTION 5         // Time to detect surface crossing at the beginning of the dive (seconds)
 #define BEGIN_SURFACE_DETECTION 0.05     // depth max-min to detect surface crossing at the beggining of the dive
-#define TIME_CHECK_POWER 10 // Time between 2 battery ccheck during dynamic dive. (seconds)
-#define LOW_BATTERY_LEVEL 3.3 //  If vBat < Low battery level, go back to sleep without water detection wakeup (Volts)
+#define TIME_CHECK_POWER 60              // Time between 2 battery ccheck during dynamic dive. (seconds)
+#define LOW_BATTERY_LEVEL 3.3            //  If vBat < Low battery level, go back to sleep without water detection wakeup (Volts)
 #define TIME_UPLOAD_OTA 1800             // Time between 2 upload and OTA check (seconds)
 #define MIN_DEPTH_CHECK_AMPLITUDE 2.0    // min depth to check amplitude of depth before ending dive. (meter)
 #define ENDING_DIVE_DEPTH_AMPLITUDE 0.08 // min depth amplitude to end dive, if depth amplitude below val, diver is out of water.
