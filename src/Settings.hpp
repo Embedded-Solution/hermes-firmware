d8dcd36c04b13d6c41fb3f77bc1d80d51c04dcd4
Embedded-Solution <63601771+Embedded-Solution@users.noreply.github.com>
2023-07-02 23:30:17 +0200

    Increase ending dive depth amplitude

--- src/Settings.hpp
+++ src/Settings.hpp
@@ -6,7 +6,7 @@
 #define SDCARD_ERROR_SLEEP 2
 #define LOW_BATT_SLEEP 3
 
-#define FIRMWARE_VERSION 3.61
+#define FIRMWARE_VERSION 3.62
 
 // Debug parameters
 #define MODE_DEBUG false
@@ -37,7 +37,7 @@
 #define TIME_UPLOAD_OTA 1800             // Time between 2 upload and OTA check (seconds)
 #define TIME_CHECK_POWER 60              // Time between 2 battery ccheck during dynamic dive. (seconds)
 #define MIN_DEPTH_CHECK_AMPLITUDE 2.0    // min depth to check amplitude of depth before ending dive. (meter)
-#define ENDING_DIVE_DEPTH_AMPLITUDE 0.08 // min depth amplitude to end dive, if depth amplitude below val, diver is out of water.
+#define ENDING_DIVE_DEPTH_AMPLITUDE 0.15 // min depth amplitude to end dive, if depth amplitude below val, diver is out of water.
 #define TIMER_DEL_CREDENTIALS 10         // Time to press config button to delete all credentials 
  
 // Upload parameters
